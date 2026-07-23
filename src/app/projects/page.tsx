208b7f9204b4a573bd55fda06c821f2bbdc31269
Bhardwaj-16 <bhardwaj.14.mit@gmail.com>
2026-07-23 23:24:54 +0530

    Changed the project refresh rate to real-time (although github rate-limits to 60 times in an hours for public APIs)

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -10,7 +10,7 @@ interface Repo {
 
 async function getRepos() {
   const res = await fetch('https://api.github.com/users/bhardwaj-16/repos', {
-    next: { revalidate: 3600 }, // 1 hour
+    cache: 'no-store', // Real-time fetching
   });
   if (!res.ok) {
     throw new Error('Failed to fetch repos');
